2bc85b6da765f705f77d86822e365659153e1bb2
Ami <1414397@rgu.ac.uk>
2016-03-23 18:40:55 +0000

    jhh

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <title>form</title>
+</head>
+<body>
+
+<form action="formres.php" method="post">
+    <label>Persons Name</label><input type="text" name="fullname">
+    <label>Gender</label><input type="radio" name="gender" value="male">
+                         <input type="radio" name="gender" value="female">
+    <label>Main Superhero Power</label><input type="text" name="power">
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
