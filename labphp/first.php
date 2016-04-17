16d646ff64902278743dbe49646ea063ed5767b5
Aaru <1414397@rgu.ac.uk>
2016-04-17 13:23:07 +0100

    jhh

---
+++ labphp/first.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aaru
+ * Date: 17/04/2016
+ * Time: 13:21
+ */
+$arr=array("A","B","C");
+echo $arr[0];
+unset $arr[1];
+echo <br>;
+echo $arr[0]."   ".$arr[1]."   ".$arr[2]."   ";
+ ?>
