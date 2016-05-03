d79c53e0a9f73cd8cc972b02d4d7b98ceedcb373
Aaru <1414397@rgu.ac.uk>
2016-05-03 14:06:11 +0100

    jhh

--- p/database/connection.php
+++ p/database/connection.php
@@ -9,10 +9,7 @@ define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'ba7adc8d866da9');
 define('DB_PASSWORD', '26b08a1c');
 define('DB_DATABASE', 'DB_PHP');
-$db    = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-
-
-}
 ?>
 
