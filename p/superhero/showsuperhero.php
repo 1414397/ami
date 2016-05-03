3341d3a4ef86da5180fbee3a5bab5e66a6556e71
Aaru <1414397@rgu.ac.uk>
2016-05-03 17:40:35 +0100

    jhh

--- p/superhero/showsuperhero.php
+++ p/superhero/showsuperhero.php
@@ -9,7 +9,7 @@ $sql="SELECT * FROM superheros" ;
 $result =mysqli_query($db, $sql);
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<p>" . $row['superheroID'] . $row['firstName'] . $row['lastName'] . $row['mainSuperpower'] ."<p>";
+        echo "<p>" . $row['superheroID']."    " . $row['firstName'] . "    " .$row['lastName'] ."    " . $row['mainSuperpower'] ."<p>";
     }
 }
 else {
