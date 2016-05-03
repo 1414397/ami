7dcd3b4876575f06747737971d86d699ffbcc4c6
Aaru <1414397@rgu.ac.uk>
2016-05-03 14:48:38 +0100

    jhh

--- first.php
+++ first.php
@@ -7,8 +7,8 @@
 <ul>
 
     <li><a href="printer.php?place=earth">Earth        </a></li>
-    <li><a href="printer.php?place=mars">     Mars   </a></li>
-    <li><a href="printer.php?place=uranus">     Uranus   </a></li>
+    <li><a href="printer.php?place=mars"> Mars   </a></li>
+    <li><a href="printer.php?place=uranus"> Uranus   </a></li>
 </ul>
 </body>
 </html>
