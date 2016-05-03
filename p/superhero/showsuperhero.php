37902e85cd84bc10abb5145289cb7355753a3828
Aaru <1414397@rgu.ac.uk>
2016-05-03 17:54:26 +0100

    jhh

--- p/superhero/showsuperhero.php
+++ p/superhero/showsuperhero.php
@@ -1,23 +1,55 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>superhero home page</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css" >
+    <link href='https://fonts.googleapis.com/css?family=Amiri:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header>
+    <h1>Superhero system</h1>
+    </p>
+</header>
+<main class="grid-container">
+    <nav class="grid-33">
+        <ul id="mainlist">
+            <li> <a href="insertsuperhero.php">Superhero form</a> </li>
+            <li> <a href="showsuperhero.php">Show Superheros</a>  </li>
+            <li> <a href="battle.php"></a> </li>
+            <li> <a href="displaybattles.php"></a>  </li>
+        </ul>
 
-include("connection.php");
+    </nav>
+    <aside class="grid-66">
+        <?php
+
+        include("connection.php");
 
 
-$sql="SELECT * FROM superheros" ;
+        $sql="SELECT * FROM superheros" ;
 
 
-$result =mysqli_query($db, $sql);
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<p>" . $row['superheroID']."    " . $row['firstName'] . "    " .$row['lastName'] ."    " . $row['mainSuperpower'] ."<p>";
-    }
-}
-else {
-    echo "0 results";
-}
+        $result =mysqli_query($db, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<p>" . $row['superheroID']."    " . $row['firstName'] . "    " .$row['lastName'] ."    " . $row['mainSuperpower'] ."<p>";
+            }
+        }
+        else {
+            echo "0 results";
+        }
 
-$result->close();
-$db->close();
+        $result->close();
+        $db->close();
 
-header("location:index.html");
-?>
+        ?>
+    </aside>
+</main>
+<footer>
+    <hr>
+    <p>Designed by[Ami Jiyani[2016]]</p>
+</footer>
+</body>
+</html>
