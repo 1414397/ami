7f533740d7af1ae939a851a0039b5fc343b3764f
Aaru <1414397@rgu.ac.uk>
2016-05-03 13:19:55 +0100

    jhh

--- p/database/display.php
+++ p/database/display.php
@@ -1,7 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Aaru
- * Date: 03/05/2016
- * Time: 11:05
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Display Movie</title>
+
+   </head>
+<body>
+<header>
+
+
+</header>
+<main>
+    <nav>
+        <ul>
+            <li><a href="first.php">Home</a></li>
+            <li><a href="display.php?display=all<">All Movies</a></li>
+            <li><a href="display.php?display=marvelstudio">Created by Marvel Movie Studio</a></li>
+            <li><a href="display.php?display=after2010">Created After 2010</a></li>
+            <li><a href="display.php?dispaly=xmemfilms">XMen Films</a></li>
+        </ul>
+
+    </nav>
+    <aside>
+        include('connection.php');
+        $display=$_GET["display"];
+        if($display=="all"){
+        $sql = "SELECT * FROM marvelmovies1";
+        }
+        else if($display=="after2010")
+        {
+        $sql = "SELECT * FROM marvelmovies1 WHERE yearReleased>2010";
+        }
+        else if($display=="xmenfilms")
+        {
+        $sql = "SELECT * FROM marvelmovies1 WHERE title="X-Men";
+        }
+        else if($display=="marvelstudio")
+        {
+        $sql = "SELECT * FROM marvelmovies1 WHERE productionStudio="Marvel Studios";
+        }
+
+
+
+        $result =mysqli_query($db, $sql);
+        if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+        echo  $row["marvelMovieID"]. "  " . $row["yearReleased"]. " " . $row["title"].  " " . $row["productionStudio"]." " . $row["notes"]."<br>";
+        }
+        } else {
+        echo "0 results";
+        }
+        ?>
+
+    </aside>
+</main>
+<footer class="grid-99">
+    <p>Designed by[Ami Jiyani[2016]]</p>
+</footer>
+</body>
+</html>
