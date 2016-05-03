677785083cce320ba9e8be36b7993fbba932916b
Aaru <1414397@rgu.ac.uk>
2016-05-03 17:38:23 +0100

    jhh

--- p/superhero/showsuperhero.php
+++ p/superhero/showsuperhero.php
@@ -7,20 +7,12 @@ $sql_query ="SELECT * FROM superheros" ;
 
 
 $result =mysqli_query($db, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
         echo "<p>" . $row['superheroID'] . $row['firstName'] . $row['lastName'] . $row['mainSuperpower'] ."<p>";
     }
-} else {
+}
+else {
     echo "0 results";
 }
-
-$result->close();
-$db->close();
-
-
-header("location:index.html");
-
-
 ?>
