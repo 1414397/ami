0baf8b2c4726c9fd2cbfa97bdc9fa318ba92c634
Aaru <1414397@rgu.ac.uk>
2016-05-03 14:01:38 +0100

    jhh

--- p/database/connection.php
+++ p/database/connection.php
@@ -11,8 +11,8 @@ define('DB_PASSWORD', '26b08a1c');
 define('DB_DATABASE', 'DB_PHP');
 $db    = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-if($db->connect_errno){
-    die('Connectfailed['.$db->connect_error.']');
+
+
 }
 ?>
 
