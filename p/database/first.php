54610c05b9a36c0bcb7e97a98fea51288cee56ff
Aaru <1414397@rgu.ac.uk>
2016-05-03 13:47:13 +0100

    jhh

--- p/database/first.php
+++ p/database/first.php
@@ -16,10 +16,10 @@
     <nav>
         <ul>
             <li><a href="first.php">Home</a></li>
-            <li><a href="display.php?display=all<">All Movies</a></li>
-            <li><a href="display.php?display=marvelstudio">Created by Marvel Movie Studio</a></li>
-            <li><a href="display.php?display=after2010">Created After 2010</a></li>
-            <li><a href="display.php?dispaly=xmemfilms">XMen Films</a></li>
+            <li><a href="display1.php?display=all<">All Movies</a></li>
+            <li><a href="display1.php?display=marvelstudio">Created by Marvel Movie Studio</a></li>
+            <li><a href="display1.php?display=after2010">Created After 2010</a></li>
+            <li><a href="display1.php?dispaly=xmemfilms">XMen Films</a></li>
         </ul>
 
     </nav>
